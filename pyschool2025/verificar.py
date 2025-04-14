f018e5c51b05143bf78492b449b103139810d604
fralfaro <francisco.alfaro.496@gmail.com>
2025-04-13 22:41:41 -0400

    update habitacion 00

--- pyschool2025/verificar.py
+++ pyschool2025/verificar.py
@@ -77,6 +77,17 @@ def salon_6(senal_3, senal_5, senal_15, senal_7):
     else:
         text("La puerta sigue cerrada... Revisa las instrucciones y verifica las respuestas esperadas.", "warning")
 
+
+def salon_00(url, df):
+    url_correcta = 'https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv'
+    if url is None or df is None:
+        text("Debes asignar un valor a las variables `url` y `df`.", "info")
+    elif url == url_correcta and len(df.columns) >= 10:
+        hyperlink("¡Correcto! Has restaurado el sistema del laboratorio. Avanza a la siguiente sala.", "end.html", "success")
+    else:
+        text("Revisa bien las instrucciones... ¿Estás seguro que cargaste los datos correctamente?", "warning")
+
+
 if __name__ == "__main__":
     salon_0("PySchool2025")
     salon_1(None)
